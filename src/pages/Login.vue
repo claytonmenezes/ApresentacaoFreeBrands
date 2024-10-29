eaec169f7e8a0be475b7ca7975302001e562a212
Clayton Menezes dos Santos <claytonmenezes@hotmail.com>
2024-10-28 21:57:30 -0300

    novos components

--- src/pages/Login.vue
+++ src/pages/Login.vue
@@ -1,7 +1,25 @@
 <template>
-    <div>
-        blablabla
+  <div class="grid grid-cols-12">
+    <div class="col-span-6 bg-zinc-900 h-screen">
+      <div class="flex items-center text-lg font-medium p-8">
+        <img src="/logo2.png" class="w-8 mr-1"/>
+        <div class="text-[#4c6c84]">
+          FreeBrands
+        </div>
+      </div>
+      <div class="text-white h-[calc(100vh-100px)] grid items-end p-8">
+          &ldquo;This library has saved me countless hours of work and
+          helped me deliver stunning designs to my clients faster than
+          ever before.&rdquo;
+          <br/>
+          <br/>
+          "Sofia Davis"
+      </div>
     </div>
+    <div class="col-span-6 bg-muted grid place-items-center">
+      asdasd
+    </div>
+  </div>
 </template>
 
 <script>
